24f30803cefedca9a43f4a143fcfae6ddf789694
aish8la <64914670+aish8la@users.noreply.github.com>
2024-05-13 23:54:42 +0500

    add operation functions to object

--- script.js
+++ script.js
@@ -3,26 +3,17 @@ let operandA = '';
 let operandB = '';
 let operationCount = 0;
 const displayBox = document.querySelector('#display');
+const operations = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+}
 
 function updateDisplay() {
     displayBox.textContent = display;
 }
 
-const add = (a, b) => {
-    return a + b;
-}
-
-const subtraction = (a, b) => {
-    return a - b;
-}
-
-const multiplication = (a, b) => {
-    return a * b;
-}
-
-const divide = (a, b) => {
-    return a / b;
-}
 
 const buttons = document.querySelectorAll('.button');
 
@@ -43,3 +34,20 @@ buttonContainer.addEventListener('click', event => {
         updateDisplay();
     }
 });
+
+
+
+
+
+buttonContainer.addEventListener('click', event => {
+    const button = event.target;
+    if (button.getAttribute('id') === 'equals-btn') {
+        operate(operandA, operandB, )
+        updateDisplay();
+    }
+});
+
+
+function operate(valueA, valueB, operation) {
+    display = operation(valueA, valueB);
+}
